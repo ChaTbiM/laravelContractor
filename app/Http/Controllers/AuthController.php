6480d043fd73fa62d16a1ed864a0b1bcbb71b2ce
mustapha chatbi <chatbimdev@gmail.com>
2020-04-19 10:47:11 +0100

    testing get user endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -86,6 +86,7 @@ class AuthController extends Controller
 
     public function getAuthenticatedUser()
     {
+        return response()->json("here", 200, $headers);
         try {
             if (!($user = JWTAuth::parseToken()->authenticate())) {
                 return response()->json(['user_not_found'], 404);
--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,9 @@ use App\User; // delete later
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->post('/user', function (Request $request) {
+//     return $request->bearerToken()();
+// });
 
 Route::post("/login", "AuthController@login");
 Route::post("/register", "AuthController@register");
